347bd7e26e6d602a46d3b4ea545d8874c5ad6914
José Ignacio Gutiérrez Guzmán <joselitosinmiedo@gmail.com>
2018-04-03 18:55:48 -0500

    Programa 1

--- POO301403/src/Uno/Uno.java
+++ POO301403/src/Uno/Uno.java
@@ -1,13 +1,42 @@
 package Uno;
 
+import java.util.Scanner;
+
 public class Uno {
 
+	public static int valorProducto;
+	
+	public static int porcentajeIva;
+	
 	/**
 	 * Programa Uno
 	 */
-	 public static void main(String[] args) {
-	
-		 System.out.print("Hola Mundo");
-	 }
-	 
+	public static void main(String[] args) {
+
+		valorProducto = 0;
+		
+		porcentajeIva = 0;
+		
+		Scanner entradaEscaner = new Scanner(System.in);
+
+		
+		System.out.println("Digite el valor del producto:");
+														
+		valorProducto =  Integer.parseInt(entradaEscaner.nextLine()); 
+
+		
+		System.out.println("Digite el porcentaje de IVA:");
+
+		porcentajeIva =  Integer.parseInt(entradaEscaner.nextLine());
+		
+		
+		
+		
+		System.out.println("El valor del producto es: $" + valorProducto);
+		
+		System.out.println("El porcentaje del IVA es: " + porcentajeIva + "%");
+		
+		System.out.println("El valor del producto con IVA es: $" + (((valorProducto/100)*porcentajeIva)+valorProducto) );
+	}
+
 }
