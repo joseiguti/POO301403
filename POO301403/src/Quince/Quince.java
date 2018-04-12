4fd5b5af79cd016cc2fe6fa90a86a142d16150b8
José Ignacio Gutiérrez Guzmán <joselitosinmiedo@gmail.com>
2018-04-12 00:11:02 -0500

    15 aun no terminado

---
+++ POO301403/src/Quince/Quince.java
@@ -0,0 +1,18 @@
+package Quince;
+
+/*
+Una tienda requiere registrar las películas que renta durante un mes, 
+para ello se requiere que realice la aplicación usando una base de datos. 
+
+La aplicación debe permitir ingresar una película, 
+eliminar una película y actualizar el título de una película haciendo uso de clases y métodos.
+*/
+
+public class Quince {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
